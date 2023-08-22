240536c7b6117a7d3fdb5059e3b1046c3bf6d200
Orlando Paternina <orlandeno13@gmail.com>
2023-08-22 10:41:18 -0500

    Icono HTML y funcionalidad de desencriptar

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ function encriptar() {
                          .replace(/o/gi, "ober")
                          .replace(/u/gi, "ufat")
 
-if (document.getElementById("texto").value !=0) {
-    texto.value = textoCifrado;
+if (texto.length !=0) {
+    document.getElementById("texto").value = textoCifrado;
     tituloMensaje.textContent = "Texto encriptado con éxito";
     parrafo.textContent = "";
     muñeco.src = "./img/encriptado.png";
@@ -24,3 +24,27 @@ if (document.getElementById("texto").value !=0) {
 }
 }
 
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+    let textoCifrado = texto
+                         .replace(/enter/gi, "i")
+                         .replace(/imes/gi, "i")
+                         .replace(/ai/gi, "a")
+                         .replace(/ober/gi, "o")
+                         .replace(/ufat/gi, "u")
+
+if (texto.length !=0) {
+    document.getElementById("texto").value  = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/desencriptar.png";
+} else {
+    muñeco.src = "./img/muneco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+    alert("Debes ingresar algo de texto");
+}
+}
